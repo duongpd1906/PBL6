1b0431129c39cefbe43a39f53363c7550e7317b8
duongpd1906 <duong.phung.dinh.1906@gmail.com>
2022-09-15 21:25:29 +0700

    [Hot fix] Fix css and format

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Chat from "./views/pages/chat";
+import Chat from "./views/pages/chat";
 import Home from "./views/pages/home";
 function App() {
     return (
         <Router>
             <Routes>
-                {/* <Route path="/chat" element={<Chat />} /> */}
+                <Route path="/chat" element={<Chat />} />
                 <Route path="/" element={<Home />} />
             </Routes>
         </Router>
---
+++ client/src/components/emoji/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./emoji.scss";
+function Emoji({ state }) {
+  return (
+    state && (
+      <div className="emoji">
+        <img
+          className="img-circle"
+          style={{ width: "35px" }}
+          src={require("../../assets/images/like.png")}
+          alt=""
+        />
+        <img
+          className="img-circle"
+          style={{ width: "35px" }}
+          src={require("../../assets/images/heart.png")}
+          alt=""
+        />
+        <img
+          className="img-circle"
+          style={{ width: "35px" }}
+          src={require("../../assets/images/haha.png")}
+        />
+        <img
+          className="img-circle"
+          style={{ width: "35px" }}
+          src={require("../../assets/images/wow.png")}
+          alt=""
+        />
+        <img
+          className="img-circle"
+          style={{ width: "35px" }}
+          src={require("../../assets/images/sad.png")}
+          alt=""
+        />
+        <img
+          className="img-circle"
+          style={{ width: "35px" }}
+          src={require("../../assets/images/angry.png")}
+          alt=""
+        />
+      </div>
+    )
+  );
+}
+
+export default Emoji;
