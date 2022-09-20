89ad7e71f94942703fd0f54d41fd022587f30b76
duongpd1906 <duong.phung.dinh.1906@gmail.com>
2022-09-20 23:04:43 +0700

    [Front end] Register and login in FE

--- client/src/context/reducers.jsx
+++ client/src/context/reducers.jsx
@@ -1,7 +1,17 @@
-import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
+import {
+    DISPLAY_ALERT,
+    CLEAR_ALERT,
+    REGISTER_USER_BEGIN,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_ERROR,
+    LOGOUT_USER,
+    LOGIN_USER_BEGIN,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_ERROR, 
+} from "./actions";
+import { initialState } from "./appContext";
 
 const reducer = (state, action) => {
-
     if (action.type === DISPLAY_ALERT) {
         return {
             ...state,
@@ -17,6 +27,68 @@ const reducer = (state, action) => {
             showAlert: false,
             alertType: "",
             alertText: "",
+        };
+    }
+
+    if (action.type === REGISTER_USER_BEGIN) {
+        return { ...state, isLoading: true };
+    }
+
+    if (action.type === REGISTER_USER_SUCCESS) {
+        return {
+            ...state,
+            user: action.payload.user,
+            token: action.payload.token,
+            isLoading: false,
+            showAlert: true,
+            alertType: "success",
+            alertText: "User Created! Redirecting...",
+        };
+    }
+
+    if (action.type === REGISTER_USER_ERROR) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: "danger",
+            alertText: action.payload.msg,
+        };
+    }
+
+    if (action.type === LOGOUT_USER) {
+        return {
+            ...initialState,
+            user: null,
+            token: null,
+        };
+    }
+
+    if (action.type === LOGIN_USER_BEGIN) {
+        return {
+            ...state,
+            isLoading: true,
+        };
+    }
+
+    if (action.type === LOGIN_USER_SUCCESS) {
+        return {
+            ...state,
+            isLoading: false,
+            user: action.payload.user,
+            token: action.payload.token,
+            showAlert: true,
+            alertType: "success",
+            alertText: "Login Successful! Redirecting...",
+        };
+    }
+    if (action.type === LOGIN_USER_ERROR) {
+        return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: "danger",
+            alertText: action.payload.msg,
         };
     }
 
