89ef96a394f1d91be2c2d4433e4b93a8799744a7
HoThiHieu <92620426+HoThiHieu@users.noreply.github.com>
2022-12-24 09:06:46 +0700

    [All] API change password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { register, login } from "../controllers/authController.js";
+import { register, login, changePassword } from "../controllers/authController.js";
+import authenticateUser from "../middleware/auth.js";
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/change-password").post(authenticateUser, changePassword);
 
 export default router;
