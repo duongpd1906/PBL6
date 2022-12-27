c2144be446e396077c88c269f17c4db3aad5a2b7
duongpd-0230 <duong1906ltv@gmail.com>
2022-12-28 01:34:44 +0700

    update socket

--- client/src/views/pages/chat/index.js
+++ client/src/views/pages/chat/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import {useLocation, useNavigate} from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import Messages from "../../../components/messenger";
 import ChatBar from "../../../components/chat-bar";
 import "./chat.scss";
@@ -12,9 +12,15 @@ function Chat(props) {
 	const [messages, setMessages] = useState([]);
 	const [newMessage, setNewMessage] = useState("");
 	const [arrivalMessage, setArrivalMessage] = useState(null);
-	const ENDPOINT = "http://localhost:5000";
+	const ENDPOINT = "http://pbl6fb.online/";
 	const socket = useRef(io(ENDPOINT));
-	const { user, conversation, getMyConversation, listConversations, getConversationFromTwoUser } = useAppContext();
+	const {
+		user,
+		conversation,
+		getMyConversation,
+		listConversations,
+		getConversationFromTwoUser,
+	} = useAppContext();
 	const scrollRef = useRef();
 	const location = useLocation();
 	const navigate = useNavigate();
@@ -28,15 +34,14 @@ function Chat(props) {
 				createdAt: Date.now(),
 			});
 		});
-		if(location.state?.friend_id){
-			getConversationFromTwoUser(user._id, location.state.friend_id)
+		if (location.state?.friend_id) {
+			getConversationFromTwoUser(user._id, location.state.friend_id);
 		}
-		
 	}, []);
 	useEffect(() => {
 		if (conversation) {
-			setCurrentChat(conversation)
-		} 
+			setCurrentChat(conversation);
+		}
 	}, [conversation]);
 	useEffect(() => {
 		arrivalMessage &&
@@ -45,22 +50,23 @@ function Chat(props) {
 	}, [arrivalMessage, currentChat]);
 
 	useEffect(() => {
-		if(currentChat){
-
+		if (currentChat) {
 			navigate("/chat", { replace: true });
 			const getMessages = async () => {
 				try {
-					const res = await axios.get("/api/message/" + currentChat?._id);
+					const res = await axios.get(
+						"/api/message/" + currentChat?._id
+					);
 					setMessages(res.data);
 				} catch (err) {
 					console.log(err);
-			}
-		};
-		getMessages();
-		currentChat.members.map((userId) => {
-			socket.current.emit("addUser", userId);
-		})
-	}
+				}
+			};
+			getMessages();
+			currentChat.members.map((userId) => {
+				socket.current.emit("addUser", userId);
+			});
+		}
 	}, [currentChat]);
 	const handleSubmit = async () => {
 		if (newMessage === "") {
@@ -99,9 +105,25 @@ function Chat(props) {
 			<div className="chat-content col-12">
 				<div className="border-right">
 					<div className="search-bar col-11 m-auto my-2">
-                        <span role="img" aria-label="search" class="anticon anticon-search"><svg viewBox="64 64 896 896" focusable="false" data-icon="search" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path></svg></span>
-                        <input type="text" placeholder="Tìm kiếm"/>
-                    </div>
+						<span
+							role="img"
+							aria-label="search"
+							class="anticon anticon-search"
+						>
+							<svg
+								viewBox="64 64 896 896"
+								focusable="false"
+								data-icon="search"
+								width="1em"
+								height="1em"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
+							</svg>
+						</span>
+						<input type="text" placeholder="Tìm kiếm" />
+					</div>
 					<div className="chat-bar">
 						{listConversations.map((c) => (
 							<div onClick={() => setCurrentChat(c)}>
@@ -119,11 +141,11 @@ function Chat(props) {
 						<Messages
 							listmessages={messages}
 							friendId={currentChat.members?.find(
-										(x) => x !== user._id
-								)}
+								(x) => x !== user._id
+							)}
 							handleSubmit={handleSubmit}
-							setNewMessage = {setNewMessage}
-							newMessage = {newMessage}
+							setNewMessage={setNewMessage}
+							newMessage={newMessage}
 							arrivalMessage={arrivalMessage}
 						/>
 					) : (
